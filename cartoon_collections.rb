1468c8c9feb7e8218f49e1cc04b6d58a13b7827a
chic-page-6502 <zacharytq@gmail.com>
2019-07-13 04:34:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,11 @@ def summon_captain_planet(array)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? do |thing|
+    thing.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
