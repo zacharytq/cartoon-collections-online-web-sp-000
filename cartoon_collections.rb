1dc241ba7045ddb0fc072242d06708359ba6f6d7
chic-page-6502 <zacharytq@gmail.com>
2019-07-13 04:21:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
@@ -13,4 +16,4 @@ end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+end 
