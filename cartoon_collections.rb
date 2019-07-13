f9ffef48aaae595a79992cc9204616ca5f080678
chic-page-6502 <zacharytq@gmail.com>
2019-07-13 04:24:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map do |call|
+    call.capitalize << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
